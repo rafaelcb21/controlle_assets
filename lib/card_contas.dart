c01e97f140162afa7732ec4086f07602eec49a44
rafaelcb21 <rafaelcb21@gmail.com>
2017-06-13 20:52:50 -0300

    version 0.0.3

---
+++ lib/card_contas.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class CardContas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.only(bottom: 6.0, right: 6.0, left: 6.0),
+      child: new  Card(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              padding: new EdgeInsets.only(left: 18.0, top: 18.0),
+              child: new Text(
+              'Contas',
+                style: new TextStyle(
+                  fontSize: 14.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF757575),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ), 
+            new ListTile(
+              leading: new CircleAvatar(
+                backgroundImage: new AssetImage('img/Caixa_Economica_Federal.svg'),
+                radius: 18.0,
+              ),
+              title: new Text(
+                'Caixa',
+                style: new TextStyle(
+                  fontSize: 13.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF212121),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: new Text(
+                'Conta corrente',
+                style: new TextStyle(
+                  fontSize: 12.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF9E9E9E)
+                ),
+              ),
+              trailing: new Text(
+                'R\$ 3.051,00',
+                style: new TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF26C6DA)
+                ),
+              )
+            ),
+
+            new Container(
+              padding: new EdgeInsets.only(left: 74.0),
+              child: new Divider(),
+            ),
+
+            new ListTile(
+              leading: new CircleAvatar(
+                backgroundImage: new AssetImage('img/Caixa_Economica_Federal.svg'),
+                radius: 21.0,
+              ),
+              title: new Text(
+                'Itau',
+                style: new TextStyle(
+                  fontSize: 13.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF212121),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: new Text(
+                'Conta corrente',
+                style: new TextStyle(
+                  fontSize: 12.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF9E9E9E)
+                ),
+              ),
+              trailing: new Text(
+                'R\$ 3.051,00',
+                style: new TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                  color: new Color(0xFF26C6DA)
+                ),
+              )
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "card_saldo.dart";
+import "card_contas.dart";
 
 void main() {
   runApp(new ControlleApp());
@@ -17,149 +19,35 @@ class ControlleApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: new Color(0xFF26C6DA),
+    appBar: new AppBar(
+      backgroundColor: new Color(0xFF26C6DA),
+    ),
+    drawer: new Drawer(
+      child: new ListView(
+        children: <Widget>[
+          new DrawerHeader(child: new Text('Header')),
+          new ListTile(
+            title: new Text('First Menu Item'),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text('Second Menu Item'),
+            onTap: () {},
+          ),
+          new Divider(),
+          new ListTile(
+            title: new Text('About'),
+            onTap: () {},
+          )
+        ],
       ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new DrawerHeader(child: new Text('Header')),
-            new ListTile(
-              title: new Text('First Menu Item'),
-              onTap: () {},
-            ),
-            new ListTile(
-              title: new Text('Second Menu Item'),
-              onTap: () {},
-            ),
-            new Divider(),
-            new ListTile(
-              title: new Text('About'),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
-      body: new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            new Container(
-              padding: new EdgeInsets.only(top: 16.0),
-              child: new Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[     
-                  new Text(
-                    'R\$  ',
-                    style: new TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF26C6DA),
-                    )
-                  ),               
-                  new Text(
-                    '3.435,23',
-                    style: new TextStyle(
-                      fontSize: 35.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF26C6DA)
-                    ),
-                  )
-                ],
-              ),
-            ),            
-            new Text(
-              'saldo geral',
-              style: new TextStyle(
-                fontSize: 12.0,
-                fontFamily: 'Roboto',
-                color: new Color(0xFF757575),
-              ),
-            ),
-            new Divider(),
-            new Text(
-              '20/05 - 19/06',
-                style: new TextStyle(
-                  fontSize: 12.0,
-                  fontFamily: 'Roboto',
-                  color: new Color(0xFF212121),
-                ),
-              ),
-            new Container(
-              padding: const EdgeInsets.only(left: 32.0, right: 32.0, top: 8.0),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Text(
-                    'Receita:',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF757575),
-                    ),
-                  ),
-                  new Text(
-                    'R\$ 3.051,00',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF66BB6A),
-                    ),                    
-                  )
-                ],
-              ),
-            ),
-            new Container(
-              padding: const EdgeInsets.only(left: 32.0, right: 32.0),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Text(
-                    'Despesa:',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF757575),
-                    ),                 
-                  ),
-                  new Text(
-                    'R\$ 2.587,00',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFFF44336),
-                    ),                    
-                  )
-                ],
-              ),
-            ),            
-            new Container(
-              padding: const EdgeInsets.only(left: 32.0, right: 32.0, bottom: 16.0),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Text(
-                    'Resultado:',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF757575),
-                    ),                  
-                  ),
-                  new Text(
-                    'R\$ 464,00',
-                    style: new TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Roboto',
-                      color: new Color(0xFF66BB6A),
-                    ),                  
-                  )
-                ],
-              ),
-            ),            
-          ],
-        ),
-      ),
-    );
+    ),
+    body: new Column(
+      children: <Widget>[
+        new CardSaldo(),
+        new CardContas(),
+      ],
+    )
+    
+  );
 }
